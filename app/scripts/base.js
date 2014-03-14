02a6d9a3fc4d3ace2dc509027337b3ccd9ce19a0
unknown <vpriscu@vpu-win.office.pitechnologies.ro>
2014-03-14 18:05:17 +0200

    Added js functionality for the future carousel.

--- app/scripts/base.js
+++ app/scripts/base.js
@@ -1,24 +1,15 @@
-$(document).ready(function () {
-  var tl = new TimelineMax();
-  jwerty.key('enter', function () { 
-    tl.to('#view-politics', 1, {display:'block'});
-  });
-  jwerty.key('ctrl', function () { 
-    tl.to('#view-sports', 1, {display:'block'});
-  });
-  jwerty.key('1', function () { 
-    tl.to('#view-technology', 1, {display:'block'});
-  });
-  jwerty.key('2', function () { 
-    tl.to('#view-business', 1, {display:'block'});
-  });
-  jwerty.key('3', function () { 
-    tl.to('#view-entertainment', 1, {display:'block'});
-  });
-  jwerty.key('4', function () { 
-    tl.to('#view-travel', 1, {display:'block'});
-  });
+$(document).ready(function () {  
+    bottom_blob();
+    carousel();
+});
+function bottom_blob() {
   $("#main_wrapper").mousemove(function(event) {
     $(".upper-blob").css("left", event.pageX + 'px');
   });
-});
+}
+function carousel() {
+  var wHeight = $(window).height();
+  var wWidth = $(window).width();
+  $('ul.carousel').css("width", wWidth - 10);
+  $('ul.carousel').css("height", wHeight);
+}
