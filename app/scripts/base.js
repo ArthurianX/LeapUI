34119f41bd9f55dfef505c4e8cadcb6605dd2045
Alin Nemes <anemes@pitechnologies.ro>
2014-03-15 01:57:37 +0200

    3rd view, needs to be made adjustments when switch to it from 2-5th slider

--- app/scripts/base.js
+++ app/scripts/base.js
@@ -2,35 +2,67 @@ $(document).ready(function () {
     bottom_blob();
     carousel();
     keybinder();
-    $('.switch-view').click(function() {
+    // Depending on which view we choose we have to initialize some things before that.
+    $('#views').change(function() {
         if (draggable[0] != undefined) {
             var type = draggable[0].vars.type;
             draggable[0].kill();
-            if (draggable[0].vars.type == "rotation") {
-                tl.to('.carousel', 0.5, {
-                    rotation: 0
-                });
-                $('.carousel').removeClass('circular');
-                carousel();
-            }
-            else {
-                $('.carousel').addClass('circular');
-                spinning_weel()
+            var carousel_element = $('.carousel');
+            var select_value = $(this).val();
+            carousel_element.removeAttr('class').addClass('carousel ' + select_value);
+            console.log(select_value);
+            switch (select_value) {
+                case 'circular':
+                    tl.to('.carousel', 0.5, {
+                        x: 0
+                    });
+                    spinning_weel();
+                    break;
+                case 'carousel':
+                    tl.to('.carousel', 0.5, {
+                        rotation: 0
+                    });
+                    carousel();
+                    break;
+                case 'cards':
+                    $('#main_wrapper').attr('style', 'overflow: visible;');
+                    $('.cards li').click(function() {
+                        if (carousel_element.hasClass('cards')) {
+                            $('#main_wrapper').removeAttr('style');
+                            carousel_element.removeAttr('class').addClass('carousel');
+                            var pos = $( ".carousel li" ).index($(this));
+                            carousel(pos);
+                            $('#views').val('carousel');
+                        }
+                    });
+                    break;
             }
         }
     });
 });
 var draggable = null;
 var tl = new TimelineMax();
+
+/**
+ * The cursor follower.
+ */
 function bottom_blob() {
     $("#main_wrapper").mousemove(function(event) {
         $(".upper-blob").css("left", event.pageX + 'px');
     });
 }
+
+/**
+ * Creates the circle view.
+ */
 function spinning_weel() {
     draggable = Draggable.create(".carousel", {type: "rotation", throwProps: true});
 }
-function carousel() {
+
+/**
+ * Creates the slider view.
+ */
+function carousel(pos) {
     var wHeight = $(window).height();
     var wWidth = $(window).width();
     $('ul.carousel').css("height", wHeight);
@@ -40,12 +72,18 @@ function carousel() {
         bounds:"#main_wrapper",
         lockAxis:true,
         throwProps:true,
-        onDragEnd: function(x) {
-            tl.to('.carousel', 0.5, {x: $().calculateX()});
+        onDragEnd: function() {
+            tl.to('.carousel', 0.1, {x: $().calculateX()});
         }
     });
+    if (pos != undefined) {
+        tl.to('.carousel', 0.1, {x: -pos * $('#main_wrapper').width()});
+    }
 }
 
+/**
+ * Enables right and left navigation by key typing.
+ */
 function keybinder() {
     jwerty.key('right/left', function (e) {
         var direction = (e.originalEvent.keyIdentifier == 'Right') ? -1 : 1;
@@ -54,6 +92,7 @@ function keybinder() {
         });
     });
 }
+
 /**
  * Calculates the X value for snap and more.
  *
