e08f3a04c13efb3bbec170c60c6dc7731d47495d
unknown <vpriscu@vpu-win.office.pitechnologies.ro>
2014-03-14 16:33:48 +0200

    Added twitter bootstrap.

--- app/scripts/base.js
+++ app/scripts/base.js
@@ -1,7 +1,21 @@
 $(document).ready(function () {
   var tl = new TimelineMax();
   jwerty.key('enter', function () { 
-    alert('this');
-    tl.to('.page', 1, {display:'block'});
+    tl.to('#view-politics', 1, {display:'block'});
+  });
+  jwerty.key('ctrl', function () { 
+    tl.to('#view-sports', 1, {display:'block'});
+  });
+  jwerty.key('1', function () { 
+    tl.to('#view-technology', 1, {display:'block'});
+  });
+  jwerty.key('2', function () { 
+    tl.to('#view-business', 1, {display:'block'});
+  });
+  jwerty.key('3', function () { 
+    tl.to('#view-entertainment', 1, {display:'block'});
+  });
+  jwerty.key('4', function () { 
+    tl.to('#view-travel', 1, {display:'block'});
   });
 });
