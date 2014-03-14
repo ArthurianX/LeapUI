4db0b9086d57326ba1f90b60fd9892c4a66edaa6
Alin Nemes <anemes@pitechnologies.ro>
2014-03-14 15:44:59 +0200

    Jquery and feeds.

---
+++ app/scripts/feeds.js
@@ -0,0 +1,34 @@
+(function ($) {
+    /**
+    * Created by anemes on 3/14/14.
+    */
+    google.load("feeds", "1");
+
+    // Our callback function, for when a feed is loaded.
+    function feedLoaded(result) {
+        if (!result.error) {
+            var query = result.query.split(' ');
+            var category = query[1];
+            var site = query[0].split(':');
+            for (var i = 0; i < result.entries.length; i++) {
+                var entry = result.entries[i];
+                var html = '<div class="'+ category + ' ' + site[1] + '">';
+                html += '<span>' + entry.title + '</span>';
+                html += '<p>' + entry.contentSnippet + '</p>' + '</div>';
+                $('#view-' + category).append(html);
+            }
+        }
+    }
+
+    function OnLoad() {
+        var categories = ['politics', 'sports', 'technology', 'business', 'entertainment', 'travel'];
+        var sites = ['site:cnn.com', "site:digg.com"];
+        for (var i = 0; i < categories.length; i++) {
+            for (var j = 0; j < sites.length; j++){
+                google.feeds.findFeeds(sites[j] + ' ' + categories[i], feedLoaded);
+            }
+        }
+    }
+
+    google.setOnLoadCallback(OnLoad);
+})(jQuery);
