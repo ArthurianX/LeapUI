b3ec9c8a1afbc3f657f248b1f81285521cbf498f
Alin Nemes <anemes@pitechnologies.ro>
2014-03-14 23:34:14 +0200

    Circles

--- app/scripts/base.js
+++ app/scripts/base.js
@@ -2,18 +2,39 @@ $(document).ready(function () {
     bottom_blob();
     carousel();
     keybinder();
+    $('.switch-view').click(function() {
+        if (draggable[0] != undefined) {
+            var type = draggable[0].vars.type;
+            draggable[0].kill();
+            if (draggable[0].vars.type == "rotation") {
+                tl.to('.carousel', 0.5, {
+                    rotation: 0
+                });
+                $('.carousel').removeClass('circular');
+                carousel();
+            }
+            else {
+                $('.carousel').addClass('circular');
+                spinning_weel()
+            }
+        }
+    });
 });
+var draggable = null;
 var tl = new TimelineMax();
 function bottom_blob() {
     $("#main_wrapper").mousemove(function(event) {
         $(".upper-blob").css("left", event.pageX + 'px');
     });
 }
+function spinning_weel() {
+    draggable = Draggable.create(".carousel", {type: "rotation", throwProps: true});
+}
 function carousel() {
     var wHeight = $(window).height();
     var wWidth = $(window).width();
     $('ul.carousel').css("height", wHeight);
-    Draggable.create(".carousel", {
+    draggable = Draggable.create(".carousel", {
         type:"x",
         edgeResistance:0.65,
         bounds:"#main_wrapper",
