042c180200893f90a1eedee43b0f6da265be64d2
Alin Nemes <anemes@pitechnologies.ro>
2014-03-14 18:14:17 +0200

    Draggable

--- app/scripts/base.js
+++ app/scripts/base.js
@@ -1,15 +1,32 @@
-$(document).ready(function () {  
+$(document).ready(function () {
     bottom_blob();
     carousel();
 });
 function bottom_blob() {
-  $("#main_wrapper").mousemove(function(event) {
-    $(".upper-blob").css("left", event.pageX + 'px');
-  });
+    $("#main_wrapper").mousemove(function(event) {
+        $(".upper-blob").css("left", event.pageX + 'px');
+    });
 }
 function carousel() {
-  var wHeight = $(window).height();
-  var wWidth = $(window).width();
-  $('ul.carousel').css("width", wWidth - 10);
-  $('ul.carousel').css("height", wHeight);
+    var wHeight = $(window).height();
+    var wWidth = $(window).width();
+    $('ul.carousel').css("width", wWidth - 10);
+    $('ul.carousel').css("height", wHeight);
+    var gridWidth = $('#main_wrapper').width();
+    var gridHeight = $('#main_wrapper').height();
+    Draggable.create(".carousel", {
+        type:"x",
+        edgeResistance:0.65,
+        bounds:"#main_wrapper",
+        lockAxis:true,
+        throwProps:true,
+        snap: {
+            x: function(endValue) {
+                return Math.round(endValue / gridWidth) * gridWidth;
+            },
+            y: function(endValue) {
+                return Math.round(endValue / gridHeight) * gridHeight;
+            }
+        }
+    });
 }
